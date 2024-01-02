0842c79552b300377cae2d664932937707908f79
imane <imanezenasnii@gmail.com>
2024-01-02 02:49:15 +0100

    append configuration lines to the SSH client configuration file

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,7 +1,5 @@
 # append configuration lines to the SSH client configuration file
 
-exec { 'configure_ssh':
-  command => 'echo "PasswordAuthentication no\nIdentityFile ~/.ssh/school" >> /etc/ssh/ssh_config',
-  path    => '/bin/',
-  creates => '/etc/ssh/ssh_config',
+exec { 'echo "PasswordAuthentication no\nIdentityFile ~/.ssh/school" >> /etc/ssh/ssh_config':
+        path    => '/bin/'
 }
