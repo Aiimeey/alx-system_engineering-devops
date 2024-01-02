b81c366504491dbf9a9f9de96caaf717aa6c4c93
imane <imanezenasnii@gmail.com>
2024-01-02 02:45:11 +0100

    append configuration lines to the SSH client configuration file

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,12 +1,13 @@
 # append configuration lines to the SSH client configuration file
+
 file_line { 'Turn off passwd auth':
   ensure => present,
-  path  => '/etc/ssh/sshd_config',
-  line  => 'PasswordAuthentication no',
+  path   => '/etc/ssh/sshd_config',
+  line   => 'PasswordAuthentication no',
 }
 
 file_line { 'Declare identity file':
   ensure => present,
-  path  => '/etc/ssh/ssh_config',
-  line  => 'IdentityFile ~/.ssh/school',
+  path   => '/etc/ssh/ssh_config',
+  line   => 'IdentityFile ~/.ssh/school',
 }
