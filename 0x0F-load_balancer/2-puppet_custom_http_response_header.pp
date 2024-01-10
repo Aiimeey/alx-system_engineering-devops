dfd91989d6cfeaa72b8c3da12170d4ed7334c538
imane <imanezenasnii@gmail.com>
2024-01-10 03:55:51 +0100

    Add load balancer

---
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -0,0 +1,12 @@
+# Install and configure Nginx web server
+exec { 'update':
+  provider => 'shell',
+  command  => 'sudo apt-get -y update',
+  before   => Exec['install Nginx'],
+}
+
+exec {'install Nginx':
+  provider => shell,
+  command  => 'sudo apt-get -y install nginx',
+  before   => Exec['add_header'],
+}
